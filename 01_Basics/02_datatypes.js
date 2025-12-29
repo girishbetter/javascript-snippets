c72984c2eed54b1036086c2a4f69100d847e4bce
Girish Chapekar <157143797+girishbetter@users.noreply.github.com>
2025-12-29 13:16:34 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all javascript code as new version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3
+    +
+    3
+)// this is not a good practice , code readability should be high
+
+console.log("Girish")
+
+let name = "ModiJi"
+let age = 60
+let isloggedIn = true
+let state;
+
+// number => 2 to power 53 is the range
+//bigint
+//string = "Girish"
+//boolean => true/false
+//null => standalone value
+//undefined => it's a value and datatype too
+//symbol => unique , mostly used in react
+
+//object
+
+console.log(name)
+console.log(typeof null)
