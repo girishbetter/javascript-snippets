9cd84f663415ca77b6667372512df7de2e246352
Girish Chapekar <157143797+girishbetter@users.noreply.github.com>
2025-12-29 13:28:01 +0000

    conversions

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Girish"
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Girish"
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+//console.log(boolIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(typeof stringNumber);
+//console.log(stringNumber)
