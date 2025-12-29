55639508490928532b4d78f87a695845f45e50e6
Girish Chapekar <157143797+girishbetter@users.noreply.github.com>
2025-12-29 03:25:13 +0000

    learn about variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1444281
+let namee = "Girish"
+var accountPassword = "11223344"
+accountCity = "Nagpur" // this is not a good practice we should always specify the type
+
+let account_Balance; // we can define a variable without initializing it with a value
+
+//accountId = 1122222
+
+//console.log(accountId);
+/*
+This is not allowed , we can't change the value of the constant variable
+*/
+
+/*
+var keyword is not used in todays world due to block scope and function scope 
+*/
+
+console.table([accountId, namee, accountPassword, accountCity, account_Balance])
