3e0e4e646bced91c662bdd5a472750ee511c5fa1
Girish Chapekar <157143797+girishbetter@users.noreply.github.com>
2025-12-30 11:14:44 +0000

    added maths portion to it

---
+++ 01_Basics/06_Numbers_and_Maths.js
@@ -0,0 +1,31 @@
+const score = 400
+
+const balance = new Number(700)
+
+// console.log(score);
+// console.log(balance)
+
+// console.log(score.toString().length)
+// console.log(score.toFixed(2))
+
+const marks = 125.8946
+//console.log(marks.toPrecision(6))
+
+/****************Maths*************************/
+// console.log(Math);
+// console.log(Math.abs(-5))
+// console.log(Math.max(-5,6,7,3,9))
+// console.log(Math.min(-5,6,7,3,9))
+// console.log(Math.round(8.9))
+// console.log(Math.ceil(8.9))
+// console.log(Math.floor(8.9))
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1); //with this we will neve get the value 0 the least value that we can get is 1
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(min+max+1))+min)
