6b087a845297991565f04dbfd85ed7939eff3999
Girish Chapekar <157143797+girishbetter@users.noreply.github.com>
2025-12-31 12:40:23 +0000

    learn about arrays in Js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+
+const arr = [1,2,3,4,5]
+
+console.log(arr.length)
+arr.push(6)
+arr.push(7)
+arr.pop()
+console.log(arr)
+arr.unshift(9) //adds the element to start of the array
+//console.log(arr)
+arr.shift() //delets the first element of the array
+console.log(arr)
+
+console.log(arr.includes(8))
+console.log(arr.indexOf(3))
